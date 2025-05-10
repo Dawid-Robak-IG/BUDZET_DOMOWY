64f7407865295fe0f0d68d6e734bd9f4fb629b15
Dawid Robak <dawid.robak1704@gmail.com>
2025-05-10 19:39:09 +0200

    data backup

---
+++ backup_baza.sql
@@ -0,0 +1,207 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: BUDZET_DOMOWY
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Budzet domowy`
+--
+
+DROP TABLE IF EXISTS `Budzet domowy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Budzet domowy` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Kwota` int NOT NULL,
+  `OperacjaID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FKBud¿et dom282223` (`OperacjaID`),
+  CONSTRAINT `FKBud¿et dom282223` FOREIGN KEY (`OperacjaID`) REFERENCES `Operacja` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Budzet domowy`
+--
+
+LOCK TABLES `Budzet domowy` WRITE;
+/*!40000 ALTER TABLE `Budzet domowy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Budzet domowy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Dziecko`
+--
+
+DROP TABLE IF EXISTS `Dziecko`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Dziecko` (
+  `Uzytkownik zalogowanyID` int NOT NULL,
+  `Saldo` int NOT NULL,
+  `Kieszonkowe` int DEFAULT NULL,
+  `ID_Rodzic1` int NOT NULL,
+  `ID_Rodzic2` int DEFAULT NULL,
+  PRIMARY KEY (`Uzytkownik zalogowanyID`),
+  KEY `FKDziecko833546` (`ID_Rodzic1`),
+  KEY `FKDziecko833545` (`ID_Rodzic2`),
+  CONSTRAINT `FKDziecko833545` FOREIGN KEY (`ID_Rodzic2`) REFERENCES `Uzytkownik zalogowany` (`ID`),
+  CONSTRAINT `FKDziecko833546` FOREIGN KEY (`ID_Rodzic1`) REFERENCES `Uzytkownik zalogowany` (`ID`),
+  CONSTRAINT `FKDziecko923729` FOREIGN KEY (`Uzytkownik zalogowanyID`) REFERENCES `Uzytkownik zalogowany` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Dziecko`
+--
+
+LOCK TABLES `Dziecko` WRITE;
+/*!40000 ALTER TABLE `Dziecko` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Dziecko` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Kategoria`
+--
+
+DROP TABLE IF EXISTS `Kategoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Kategoria` (
+  `Nazwa` varchar(255) NOT NULL,
+  `Uzytkownik zalogowanyID` int NOT NULL,
+  PRIMARY KEY (`Nazwa`),
+  KEY `FKKategoria 746861` (`Uzytkownik zalogowanyID`),
+  CONSTRAINT `FKKategoria 746861` FOREIGN KEY (`Uzytkownik zalogowanyID`) REFERENCES `Uzytkownik zalogowany` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Kategoria`
+--
+
+LOCK TABLES `Kategoria` WRITE;
+/*!40000 ALTER TABLE `Kategoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Kategoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Operacja`
+--
+
+DROP TABLE IF EXISTS `Operacja`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Operacja` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Kwota` int NOT NULL,
+  `Data` date NOT NULL,
+  `Notatka` int DEFAULT NULL,
+  `Uzytkownik zalogowanyID` int NOT NULL,
+  `Kategoria Nazwa` varchar(255) NOT NULL,
+  `czy_z_cyklicznego` bit(1) NOT NULL,
+  `Operacja cyklicznaID` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FKOperacja423377` (`Uzytkownik zalogowanyID`),
+  KEY `FKOperacja368305` (`Kategoria Nazwa`),
+  KEY `FKOperacja232880` (`Operacja cyklicznaID`),
+  CONSTRAINT `FKOperacja232880` FOREIGN KEY (`Operacja cyklicznaID`) REFERENCES `Operacja cykliczna` (`ID`),
+  CONSTRAINT `FKOperacja368305` FOREIGN KEY (`Kategoria Nazwa`) REFERENCES `Kategoria` (`Nazwa`),
+  CONSTRAINT `FKOperacja423377` FOREIGN KEY (`Uzytkownik zalogowanyID`) REFERENCES `Uzytkownik zalogowany` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Operacja`
+--
+
+LOCK TABLES `Operacja` WRITE;
+/*!40000 ALTER TABLE `Operacja` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Operacja` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Operacja cykliczna`
+--
+
+DROP TABLE IF EXISTS `Operacja cykliczna`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Operacja cykliczna` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Kwota` int NOT NULL,
+  `Data` date NOT NULL,
+  `Notatka` varchar(255) DEFAULT NULL,
+  `Czestotliwosc` varchar(255) NOT NULL,
+  `Uzytkownik zalogowanyID` int NOT NULL,
+  `Kategoria Nazwa` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FKOperacja c774395` (`Uzytkownik zalogowanyID`),
+  KEY `FKOperacja c719323` (`Kategoria Nazwa`),
+  CONSTRAINT `FKOperacja c719323` FOREIGN KEY (`Kategoria Nazwa`) REFERENCES `Kategoria` (`Nazwa`),
+  CONSTRAINT `FKOperacja c774395` FOREIGN KEY (`Uzytkownik zalogowanyID`) REFERENCES `Uzytkownik zalogowany` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Operacja cykliczna`
+--
+
+LOCK TABLES `Operacja cykliczna` WRITE;
+/*!40000 ALTER TABLE `Operacja cykliczna` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Operacja cykliczna` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Uzytkownik zalogowany`
+--
+
+DROP TABLE IF EXISTS `Uzytkownik zalogowany`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Uzytkownik zalogowany` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Imie` varchar(255) NOT NULL,
+  `Nazwisko` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Haslo` varchar(255) NOT NULL,
+  `Czy_zablokowany` bit(1) NOT NULL,
+  `Data urodzenia` date NOT NULL,
+  `Rola` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Uzytkownik zalogowany`
+--
+
+LOCK TABLES `Uzytkownik zalogowany` WRITE;
+/*!40000 ALTER TABLE `Uzytkownik zalogowany` DISABLE KEYS */;
+INSERT INTO `Uzytkownik zalogowany` VALUES (1,'Janusz','Januszewski','januszek@gmail.com','taktoja',_binary '\0','2021-01-20','Użytkownik'),(2,'mikolaj','wielki','mikolajek@gmail.com','asdasdasd',_binary '\0','2025-05-10','Użytkownik');
+/*!40000 ALTER TABLE `Uzytkownik zalogowany` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-10 19:38:31
